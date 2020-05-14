cbcd7b40ac0cee45951535b736f9b0abcadfe6bd
John Lucker <john.lucker@comcast.net>
2020-05-14 11:30:37 -0400

    Got GameBoard rendering and displaying
    Fixed the issue where the GameBoard was not rendering by taking the
    definition of the boardJsx out of the useEffect function.
    Created a helper function for replacing any empty values in a board
    array with empty strings because .map doesn't work on empty values in
    an array.
    Setup cells to be input elements with a type of number, a min value of
    1 and a max value of 9 to reflect that data that will be entered.
    Styled the board as an HTML grid with 9 columns.

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -2,32 +2,39 @@ import React, { useState, useEffect } from 'react'
 
 const GameBoard = ({ user, startingBoard }) => {
   const [board, setBoard] = useState([])
-  let boardJsx
   useEffect(() => {
-    console.log('GameBoard effect function')
-    console.log('Board is, ', startingBoard)
-    const newBoardJsx = new Array(81)
-    boardJsx = newBoardJsx.map((cell, index) => (
-      <input
-        key={index}
-        id={index}
-        className="cell"
-        value={startingBoard[index]}
-        onChange={handleChange}
-      />
-    ))
-    console.log(boardJsx)
+    console.log('GameBoard effect')
+    setBoard(startingBoard)
   }, [startingBoard])
   const handleChange = event => {
-    const newBoard = board.map((value, index) => {
-      if (event.target.id === index) {
-        value = event.target.value
-      }
-    })
+    console.log(event.target.id)
+    console.log(event.target.value)
+    const newBoard = cleanBoard.map(value => value)
+    if (event.target.value) {
+      newBoard[event.target.id] = Number(event.target.value)
+    } else {
+      newBoard[event.target.id] = ''
+    }
+    console.log(newBoard)
     setBoard(newBoard)
   }
+  const cleanBoard = [...board].map(cell => typeof cell === 'number' ? cell : '')
+  console.log('Board is, ', cleanBoard)
+  const boardJsx = cleanBoard.map((cell, index) => (
+    <input
+      key={index}
+      id={index}
+      className="cell"
+      type="number"
+      min="1"
+      max="9"
+      value={cell}
+      onChange={handleChange}
+    />
+  ))
+  console.log(boardJsx)
   return (
-    <form>
+    <form className='board'>
       {boardJsx}
     </form>
   )
