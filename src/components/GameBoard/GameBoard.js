8512867f4b9b9ddf6f60922f398561828c00f307
John Lucker <john.lucker@comcast.net>
2020-05-28 14:43:43 -0400

    Added Move validation
    Created a function to check if the current board violates any of the
    rules of a Sudoku.
    Updated the on change function to check that each move is valid before
    updating the state.
    Added a function to check if the user has solved the puzzle.

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import apiUrl from '../../apiConfig'
 import axios from 'axios'
-// import checkBoard from './checkBoard'
+import checkBoard from './checkBoard'
 
 const GameBoard = ({ user, gameId, startingBoard, msgAlert }) => {
   const [board, setBoard] = useState([])
@@ -15,28 +15,42 @@ const GameBoard = ({ user, gameId, startingBoard, msgAlert }) => {
     } else {
       newBoard[event.target.id] = ''
     }
-    setBoard(newBoard)
-    axios({
-      method: 'patch',
-      url: apiUrl + '/games/' + gameId,
-      data: {
-        'game': {
-          'board': newBoard,
-          'user_id': user.id
-        }
-      },
-      headers: {
-        Authorization: 'Token token=' + user.token
-      }
-    })
-      // .then(res => checkBoard(res.data.game.board))
-      .catch(error => {
+    if (checkBoard(newBoard)) {
+      setBoard(newBoard)
+      if (!newBoard.includes('')) {
         msgAlert({
-          heading: 'Game Update Failed with error: ' + error.message,
-          message: 'Your was unable to be updated.',
-          variant: 'danger'
+          heading: 'You Won!!!',
+          message: 'Congratulations, you have solved this puzzle!',
+          variant: 'success'
         })
+      }
+      axios({
+        method: 'patch',
+        url: apiUrl + '/games/' + gameId,
+        data: {
+          'game': {
+            'board': newBoard,
+            'user_id': user.id
+          }
+        },
+        headers: {
+          Authorization: 'Token token=' + user.token
+        }
       })
+        .catch(error => {
+          msgAlert({
+            heading: 'Game Update Failed with error: ' + error.message,
+            message: 'Your was unable to be updated.',
+            variant: 'danger'
+          })
+        })
+    } else {
+      msgAlert({
+        heading: 'Invalid Move',
+        message: 'That number already exists in that row, column, or section.',
+        variant: 'warning'
+      })
+    }
   }
   const cleanBoard = [...board].map(cell => typeof cell === 'number' ? cell : '')
   const boardJsx = cleanBoard.map((cell, index) => (
--- src/components/GameBoard/checkBoard.js
+++ src/components/GameBoard/checkBoard.js
@@ -4,16 +4,21 @@ const checkBoard = (board) => {
   return checkRows(board) && checkColumns(board) && checkSections(board)
 }
 // checks an array of arrays to see if any of the nested arrays contain duplicate values
+
 const checkDups = (array) => {
+  let noDups = true
   array.forEach(row => {
     for (let i = 1; i < 10; i++) {
       if (row.filter(cell => cell === i).length > 1) {
-        return false
+        noDups = false
       }
     }
   })
-  return true
+  return noDups
 }
+
+// takes an array representation of a sudoku board, and parses it into it's rows
+// after converting it to an array of arrays, passes to checkDups to see there are dups in any of the rows
 const checkRows = (board) => {
   const rows = []
   for (let i = 0; i < 9; i++) {
@@ -21,65 +26,80 @@ const checkRows = (board) => {
   }
   return checkDups(rows)
 }
+
+// takes an array representation of a sudoku board, and parses it into it's columns
+// after converting it to an array of arrays, passes to checkDups to see there are dups in any of the columns
 const checkColumns = (board) => {
   const columns = [[], [], [], [], [], [], [], [], []]
-  board.forEach(row => {
-    console.log(row)
-    row.forEach((cell, index) => {
-      columns[index].push(cell)
-    })
+  board.forEach((cell, index) => {
+    columns[index % 9].push(cell)
   })
   return checkDups(columns)
 }
-const checkSections = (board) => {
+
+// takes an array representation of a sudoku board, and parses it into it's componenet 3x3 squares
+const parseSections = (board) => {
   const sections = [[], [], [], [], [], [], [], [], []]
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
-      sections[j + i * 3].push(sections.slice(0, 2))
-      sections[j + i * 3 + 1].push(sections.slice(3, 5))
-      sections[j + i * 3 + 2].push(sections.slice(6, 8))
+      const start = j * 9 + i * 27
+      sections[i * 3].push(board[start])
+      sections[i * 3].push(board[start + 1])
+      sections[i * 3].push(board[start + 2])
+      sections[i * 3 + 1].push(board[start + 3])
+      sections[i * 3 + 1].push(board[start + 4])
+      sections[i * 3 + 1].push(board[start + 5])
+      sections[i * 3 + 2].push(board[start + 6])
+      sections[i * 3 + 2].push(board[start + 7])
+      sections[i * 3 + 2].push(board[start + 8])
     }
   }
+  return sections
+}
+
+// after converting it to an array of arrays, passes to checkDups to see there are dups in any of the sections
+const checkSections = (board) => {
+  const sections = parseSections(board)
   return checkDups(sections)
 }
 
-const goodBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9,
-  7, 8, 9, 1, 2, 3, 4, 5, 6,
-  4, 5, 6, 7, 8, 9, 1, 2, 3,
-  9, 1, 2, 3, 4, 5, 6, 7, 8,
-  6, 7, 8, 9, 1, 2, 3, 4, 5,
-  3, 4, 5, 6, 7, 8, 9, 1, 2,
-  8, 9, 1, 2, 3, 4, 5, 6, 7,
-  5, 6, 7, 8, 9, 1, 2, 3, 4,
-  2, 3, 4, 5, 6, 7, 8, 9, 1]
+// const goodBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9,
+//   7, 8, 9, 1, 2, 3, 4, 5, 6,
+//   4, 5, 6, 7, 8, 9, 1, 2, 3,
+//   9, 1, 2, 3, 4, 5, 6, 7, 8,
+//   6, 7, 8, 9, 1, 2, 3, 4, 5,
+//   3, 4, 5, 6, 7, 8, 9, 1, 2,
+//   8, 9, 1, 2, 3, 4, 5, 6, 7,
+//   5, 6, 7, 8, 9, 1, 2, 3, 4,
+//   2, 3, 4, 5, 6, 7, 8, 9, 1]
 
-const badSection = [1, 2, 3, 4, 5, 6, 7, 8, 9,
-  7, 8, 9, 1, 2, 3, 4, 5, 6,
-  9, 1, 2, 3, 4, 5, 6, 7, 8,
-  4, 5, 6, 7, 8, 9, 1, 2, 3,
-  6, 7, 8, 9, 1, 2, 3, 4, 5,
-  3, 4, 5, 6, 7, 8, 9, 1, 2,
-  8, 9, 1, 2, 3, 4, 5, 6, 7,
-  5, 6, 7, 8, 9, 1, 2, 3, 4,
-  2, 3, 4, 5, 6, 7, 8, 9, 1]
+// const badSection = [1, 2, 3, 4, 5, 6, 7, 8, 9,
+//   7, 8, 9, 1, 2, 3, 4, 5, 6,
+//   9, 1, 2, 3, 4, 5, 6, 7, 8,
+//   4, 5, 6, 7, 8, 9, 1, 2, 3,
+//   6, 7, 8, 9, 1, 2, 3, 4, 5,
+//   3, 4, 5, 6, 7, 8, 9, 1, 2,
+//   8, 9, 1, 2, 3, 4, 5, 6, 7,
+//   5, 6, 7, 8, 9, 1, 2, 3, 4,
+//   2, 3, 4, 5, 6, 7, 8, 9, 1]
 
-const badColumn = [1, 2, 3, 4, 5, 6, 7, 8, 9,
-  7, 8, 9, 1, 2, 3, 4, 5, 6,
-  4, 5, 6, 7, 8, 9, 1, 2, 3,
-  9, 1, 2, 3, 4, 5, 6, 7, 8,
-  6, 7, 8, 9, 1, 2, 3, 4, 5,
-  3, 4, 5, 6, 7, 8, 9, 1, 2,
-  1, 9, 8, 2, 3, 4, 5, 6, 7,
-  5, 6, 7, 8, 9, 1, 2, 3, 4,
-  2, 3, 4, 5, 6, 7, 8, 9, 1]
+// const badColumn = [1, 2, 3, 4, 5, 6, 7, 8, 9,
+//   7, 8, 9, 1, 2, 3, 4, 5, 6,
+//   4, 5, 6, 7, 8, 9, 1, 2, 3,
+//   9, 1, 2, 3, 4, 5, 6, 7, 8,
+//   6, 7, 8, 9, 1, 2, 3, 4, 5,
+//   3, 4, 5, 6, 7, 8, 9, 1, 2,
+//   1, 9, 8, 2, 3, 4, 5, 6, 7,
+//   5, 6, 7, 8, 9, 1, 2, 3, 4,
+//   2, 3, 4, 5, 6, 7, 8, 9, 1]
 
-const badRow = [1, 2, 3, 4, 5, 6, 7, 8, 9,
-  7, 8, 9, 1, 2, 3, 4, 5, 6,
-  4, 5, 6, 7, 8, 9, 1, 2, 3,
-  9, 1, 2, 3, 4, 5, 2, 7, 8,
-  6, 7, 8, 9, 1, 2, 3, 4, 5,
-  3, 4, 5, 6, 7, 8, 9, 1, 2,
-  8, 9, 1, 2, 3, 4, 5, 6, 7,
-  5, 6, 7, 8, 9, 1, 6, 3, 4,
-  2, 3, 4, 5, 6, 7, 8, 9, 1]
+// const badRow = [1, 2, 3, 4, 5, 6, 7, 8, 9,
+//   7, 8, 9, 1, 2, 3, 4, 5, 6,
+//   4, 5, 6, 7, 8, 9, 1, 2, 3,
+//   9, 1, 2, 3, 4, 5, 2, 7, 8,
+//   6, 7, 8, 9, 1, 2, 3, 4, 5,
+//   3, 4, 5, 6, 7, 8, 9, 1, 2,
+//   8, 9, 1, 2, 3, 4, 5, 6, 7,
+//   5, 6, 7, 8, 9, 1, 6, 3, 4,
+//   2, 3, 4, 5, 6, 7, 8, 9, 1]
 export default checkBoard
